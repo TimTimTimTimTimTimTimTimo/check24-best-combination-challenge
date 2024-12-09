4a376e4e968a543aa44e150428d82cce774be393
Yude Jiang <justdoingmywork@proton.me>
2024-12-09 21:50:11 +0100

    added remaining test/benchmark cases from problem statement

--- src-tauri/benches/best-combination.rs
+++ src-tauri/benches/best-combination.rs
@@ -1,15 +1,29 @@
 use std::hint::black_box;
 
-use best_combination_lib::{best_combination_single, load_data};
+use best_combination_lib::{
+    best_combination_all, best_combination_multi_1, best_combination_multi_2,
+    best_combination_single, load_data,
+};
 use betting_game_data::Data;
 use tango_bench::{benchmark_fn, tango_benchmarks, tango_main, IntoBenchmarks};
 
 fn best_combination_benchmarks() -> impl IntoBenchmarks {
     let data: &'static Data = Box::leak(Box::new(load_data()));
 
-    return [benchmark_fn("benchmark_single", |b| {
-        b.iter(|| best_combination_single(black_box(data)))
-    })];
+    return [
+        benchmark_fn("benchmark_single", |b| {
+            b.iter(|| best_combination_single(black_box(data)))
+        }),
+        benchmark_fn("benchmark_multi_1", |b| {
+            b.iter(|| best_combination_multi_1(black_box(data)))
+        }),
+        benchmark_fn("benchmark_all", |b| {
+            b.iter(|| best_combination_all(black_box(data)))
+        }),
+        benchmark_fn("benchmark_multi_2", |b| {
+            b.iter(|| best_combination_multi_2(black_box(data)))
+        }),
+    ];
 }
 
 tango_benchmarks!(best_combination_benchmarks());
--- src-tauri/tests/best-combination.rs
+++ src-tauri/tests/best-combination.rs
@@ -12,3 +12,50 @@ fn test_best_combination_single() {
         total_price: 6000
         "###);
 }
+
+#[test]
+fn test_best_combination_multi_1() {
+    let data = load_data();
+    insta::assert_yaml_snapshot!(best_combination_multi_1(&data), @r###"
+    package_ids:
+      - 0
+      - 29
+    live_coverage: 208
+    highlights_coverage: 211
+    total_coverage: 211
+    total_price: 6992
+    "###);
+}
+
+#[test]
+fn test_best_combination_all() {
+    let data = load_data();
+    insta::assert_yaml_snapshot!(best_combination_all(&data), @r###"
+    package_ids:
+      - 0
+      - 29
+      - 35
+      - 17
+      - 24
+      - 1
+    live_coverage: 5255
+    highlights_coverage: 7226
+    total_coverage: 7226
+    total_price: 8192
+    "###);
+}
+
+#[test]
+fn test_best_combination_multi_2() {
+    let data = load_data();
+    insta::assert_yaml_snapshot!(best_combination_multi_2(&data), @r###"
+    package_ids:
+      - 16
+      - 35
+      - 24
+    live_coverage: 116
+    highlights_coverage: 116
+    total_coverage: 116
+    total_price: 3199
+    "###);
+}
